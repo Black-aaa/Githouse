5b9539667f79c0ca0ef2a3fa1f949865ff1cf4ce
Black-aaa <cjy_816st@163.com>
2024-08-06 14:41:50 +0800

    test

---
+++ pro_test/build/CMakeFiles/HelloWorld.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.cpp.dir/HelloWorld.cpp.o"
+  "CMakeFiles/HelloWorld.cpp.dir/HelloWorld.cpp.o.d"
+  "HelloWorld.cpp"
+  "HelloWorld.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
